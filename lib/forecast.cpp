cba67af290b1869deafc97fe70242341d09685a6
Danil Abdulminev <danil.abdulminev@mail.ru>
2024-02-16 12:23:55 +0300

    added some func

--- lib/forecast.cpp
+++ lib/forecast.cpp
@@ -1,2 +1,42 @@
 #include "forecast.h"
 
+void PrintError(const std::string& text) {
+    std::cerr << text << '\n';
+    exit(EXIT_FAILURE);
+}
+
+json ReadConfig(const std::string& directory){
+    std::filesystem::path path = directory;
+    std::ifstream input_config(path);
+    if(input_config){
+        json data = json::parse(input_config);
+        return data;
+    }
+    PrintError("Not found config.json");
+}
+
+void GetCoords(CityInfo& city) { 
+    std::string url = url_city_template + city.name;
+    cpr::Response response = cpr::Get(cpr::Url(url), cpr::Header{{"X-Api-Key ", "49Y8Nc078fsxTIB8UgYZ2w==5yXdu1a2oKJcnwdb"}});
+    if (response.status_code == 200){
+        int16_t len = response.text.length();
+        response.text = response.text.substr(1, len-2);
+        json data = json::parse(response.text);
+        city.latitude = data["latitude"];
+        city.longitude = data["longitude"];
+    }
+    PrintError("Bad Gateway!"); 
+}
+
+void SendRequest(CityInfo& city){
+    std::string url = url_forecast_template;
+    cpr::Response response = cpr::Get(cpr::Url(url));
+    if (response.status_code == 200){
+        int16_t len = response.text.length();
+        response.text = response.text.substr(1, len-2);
+        json data = json::parse(response.text);
+        std:: cout << data << '\n';
+    }
+    PrintError("Error!");
+
+}
--- lib/forecast.h
+++ lib/forecast.h
@@ -1,7 +1,23 @@
-#include <iostream>
-#include <fstream>
-#include <filesystem>
-#include <vector>
-#include <string>
 #include <cpr/cpr.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
+
+using json = nlohmann::json;
+
+const std::string url_city_template = "https://api.api-ninjas.com/v1/city?name=";
+const std::string url_forecast_template = "https://api.open-meteo.com/v1/forecast?latitude=59.94&longitude=30.31&hourly=temperature_2m&forecast_days=16";
+
+struct CityInfo {
+  std::string name;
+  double longitude;
+  double latitude;
+};
+
+void PrintError(const std::string& text );
+
+void GetCoords (CityInfo & city);
