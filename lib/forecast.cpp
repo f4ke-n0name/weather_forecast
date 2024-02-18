594215f38dd84c6583b9582ac176cc3cfc8ea9e0
Danil Abdulminev <danil.abdulminev@mail.ru>
2024-02-18 03:54:05 +0300

    added working GetInfoForForecast

--- lib/forecast.cpp
+++ lib/forecast.cpp
@@ -10,16 +10,18 @@ json ReadConfig(const std::string& directory) {
   std::ifstream input_config(path);
   if (input_config) {
     json data = json::parse(input_config);
+    api_ninjas_city_key = data["X-Api-Key"];
+    count_days = data["forecast_days"];
     return data;
   }
   PrintError("Not found config.json");
 }
 
 void GetCoords(CityInfo& city) {
-  std::string url = url_city_template + city.name;
-  cpr::Response response = cpr::Get(
-      cpr::Url(url),
-      cpr::Header{{"X-Api-Key", "49Y8Nc078fsxTIB8UgYZ2w==5yXdu1a2oKJcnwdb"}});
+  std::string url = url_city_template;
+  cpr::Response response =
+      cpr::Get(cpr::Url(url), cpr::Parameters{{"name", city.name}},
+               cpr::Header{{"X-Api-Key", api_ninjas_city_key}});
   if (response.status_code == 200) {
     response.text = response.text.substr(
         to_parse_coords_begin, response.text.length() - to_parse_coords_end);
@@ -31,11 +33,17 @@ void GetCoords(CityInfo& city) {
   PrintError("Bad Gateway!");
 }
 
-json SendRequest(CityInfo& city) {
-  std::string url = url_forecast_template + std::to_string(city.latitude) +
-                    "&longitude=" + std::to_string(city.longitude) +
-                    "&hourly=temperature_2m&forecast_days=3";
-  cpr::Response response = cpr::Get(cpr::Url(url));
+json SendRequest(CityInfo& city, json& data) {
+  cpr::Response response =
+      cpr::Get(cpr::Url(url_forecast_template),
+               cpr::Parameters{{"longitude", std::to_string(city.longitude)},
+                               {"latitude", std::to_string(city.latitude)},
+                               {"forecast_days", std::to_string(count_days)},
+                               {"hourly", "weather_code"},
+                               {"hourly", "temperature_2m"},
+                               {"hourly", "wind_speed_10m"},
+                               {"hourly", "precipitation"},
+                               {"hourly", "precipitation_probability"}});
   if (response.status_code == 200) {
     json data = json::parse(response.text);
     return data;
@@ -43,15 +51,35 @@ json SendRequest(CityInfo& city) {
   PrintError("Error!");
 }
 
+void ParseRequest(CityInfo& city, json& info) {
+  uint16_t time_iter = 0;
+  for (uint16_t i = 0; i < count_days; ++i, time_iter += 24) {
+    for (uint16_t j = 0; j < 6; ++j) {
+      
+    }
+    for (uint16_t j = 6; j < 12; ++j) {
+    }
+    for (uint16_t j = 12; j < 18; ++j) {
+    }
+    for (uint16_t j = 18; j < 24; ++j) {
+    }
+    city_forecast[city.name][i].date = std::string format =
+        info["hourly"]["time"][time_iter].substr(0, 10);
+  }
+}
+
 void GetInfoForForecast(const std::string& directory) {
   json data_from_config = ReadConfig(directory);
-  for (auto element : data_from_config["cities"]) {
-    city_forecast[element] =
-        std::vector<AllDayWetherInfo>(data_from_config["count_days"]);
-    geographical_info[element].name = element;
-    GetCoords(geographical_info[element]);
-    std::cout << geographical_info[element].name << ' '
-              << geographical_info[element].latitude << ' '
-              << geographical_info[element].longitude << '\n';
+  for (auto city : data_from_config["cities"]) {
+    city_forecast[city] =
+        std::vector<AllDayWetherInfo>(data_from_config["forecast_days"]);
+    geographical_info[city].name = city;
+    GetCoords(geographical_info[city]);
+    std::cout << geographical_info[city].name << ' '
+              << geographical_info[city].latitude << ' '
+              << geographical_info[city].longitude << '\n';
+    json info = SendRequest(geographical_info[city], data_from_config);
+    std::cout << info << '\n' << '\n';
+    ParseRequest(geographical_info[city], info);
   }
 }
--- lib/forecast.h
+++ lib/forecast.h
@@ -10,13 +10,14 @@
 
 using json = nlohmann::json;
 
-const std::string url_city_template =
-    "https://api.api-ninjas.com/v1/city";
+const std::string url_city_template = "https://api.api-ninjas.com/v1/city";
 const std::string url_forecast_template =
     "https://api.open-meteo.com/v1/forecast";
+static std::string api_ninjas_city_key;
 
 const int16_t to_parse_coords_begin = 1;
 const int16_t to_parse_coords_end = 2;
+static uint16_t count_days;
 
 struct CityInfo {
   std::string name;
@@ -25,23 +26,24 @@ struct CityInfo {
 };
 
 struct WeatherInfo {
-  std::string date;
-  int16_t temperature;
-  std::string wind_direction;
-  int16_t wind_speed;
-  int16_t fallout;
-  int16_t wet;
+  std::string status = "";
+  int16_t max_temperature = INT_MIN;
+  int16_t min_temperature = INT_MAX;
+  int16_t wind_speed = INT_MIN;
+  int16_t precipitation = INT_MIN;
+  int16_t precipitation_probability = INT_MIN;
 };
 
 struct AllDayWetherInfo {
+  std::string date;
   WeatherInfo morning;
   WeatherInfo day;
   WeatherInfo evening;
   WeatherInfo night;
 };
 
-static std::map<std::string , std::vector<AllDayWetherInfo>> city_forecast;
-static std::map<std::string , CityInfo> geographical_info;
+static std::map<std::string, std::vector<AllDayWetherInfo>> city_forecast;
+static std::map<std::string, CityInfo> geographical_info;
 
 json ReadConfig(const std::string& directory);
 
@@ -49,5 +51,6 @@ void PrintError(const std::string& text);
 
 void GetCoords(CityInfo& city);
 
-json SendRequest(CityInfo& city);
+json SendRequest(CityInfo& city, json& data);
+
 void GetInfoForForecast(const std::string& directory);
